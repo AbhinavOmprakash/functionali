a633e665789f52dcba3aeddf3aa9ef9b74a92da3
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2021-06-04 19:47:45 +0530

    added other functions

---
+++ pyfunc/dict_functions.py
@@ -0,0 +1,6 @@
+""" functions to work with dictionaries"""
+
+
+def keys(dictionary:dict)-> tuple:
+    """Return a tuple of the keys in a dictionary."""
+    return dictionary.keys()
---
+++ pyfunc/seq_transform.py
@@ -0,0 +1,43 @@
+"""Functions that transform sequences"""
+from typing import Callable, Iterable, Generator
+
+def fmap(function:Callable, arguments:Iterable, *args) -> Generator:
+    """Similar to Python's map, but combines star_map."""
+    pass
+
+def s_fmap(function:Callable, arguments:Iterable, *args) -> tuple:
+    """Strict version of fmap()"""
+    pass
+
+def argmap(functions:Iterable[Callable], arg:Any, *args)-> Generator:
+    """Maps an argument(s) to multiple functions."""
+    pass
+
+def s_argmap(functions:Iterable[Callable], arg:Any, *args)-> tuple:
+    """Strict version of argmap()"""
+    pass
+
+def argzip(sequence:Iterable[Callable], arg:Any, *args) -> Generator:
+    """Similar to zip, but instead of zipping iterables, 
+    It zips An argument with all the values of their iterable.
+    for example argzip([1,2,3], "number") -> ( (1,"number"), (2,"number"),(3,"number") )"""
+    pass
+
+def s_argzip(sequence:Iterable[Callable], arg:Any, *args) -> tuple:
+    """strict version of argzip."""
+    pass    
+
+def unzip(sequence:Iterable)->Generator:
+    """Opposite of zip. Unzip is shallow."""
+    pass    
+
+def interleave(seq1:Iterable, seq2:Iterable, *seqs:Iterable) -> Generator:
+    """Similar to clojure's interleave."""
+    pass    
+
+def flatten(sequence:Iterable)->Generator:
+    """ Similar to clojure's flatten. Returns the contents of a nested sequence as a flat sequence"""
+    pass
+
+
+
