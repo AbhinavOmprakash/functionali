a3db22a9bee71ae30250f427081005ffd2cb3b06
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2021-06-06 11:52:44 +0530

    implemented curry

--- pyfunc/function_transform.py
+++ pyfunc/function_transform.py
@@ -1,11 +1,23 @@
 """higher order functions that consume other functions and return a modified function."""
 
 from typing import Callable
+from inspect import signature
 
 
 def curry(fn: Callable) -> Callable:
     """Returns a curried version of the function."""
-    pass
+    num_args = len(signature(fn).parameters)
+
+    def curried(arg, fn, num_args):
+        if num_args <= 1:
+            # call fn if the final arg is passed
+            return fn(arg)
+        else:
+            # call partial fn if not final arg
+            f = partial(fn, arg)
+            return lambda arg: curried(arg, f, num_args - 1)
+
+    return lambda arg: curried(arg, fn, num_args)
 
 
 def partial(fn: Callable, *args, **kwargs) -> Callable:
@@ -13,6 +25,6 @@ def partial(fn: Callable, *args, **kwargs) -> Callable:
     That will consume the remaining arguments and call the function"""
 
     def partial_fn(*rem_args, **rem_kwargs):
-        return fn(*args, *rem_args, **kwargs ** rem_kwargs)
+        return fn(*args, *rem_args, **kwargs, **rem_kwargs)
 
     return partial_fn
