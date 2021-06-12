a745660d572abdb236aa2a3e03114da6e1af87ef
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2021-06-12 20:39:47 +0530

    added imports

---
+++ functionali/__init__.py
@@ -0,0 +1,7 @@
+from .dict_functions import *
+from .function_transform import *
+from .operators import *
+from .predicates import *
+from .seq_transform import *
+from .seq_traverse import *
+from .statements import *
