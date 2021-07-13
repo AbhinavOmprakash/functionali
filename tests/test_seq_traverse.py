1e6f199103eb7e14162a298fc11034b079e85622
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2021-07-13 18:19:08 +0530

    added tests fixes #1

--- tests/test_seq_traverse.py
+++ tests/test_seq_traverse.py
@@ -22,7 +22,7 @@ def test_first():
 def test_ffirst():
     assert 1 == ffirst([[1], [2], [3]])
     assert 1 == ffirst(((1,), (2,), (3,)))
-    assert 1 == ffirst(set([(1, 2), (3, 4), (4, 5)]))
+    assert 1 == ffirst(set([(1, 2), (3, 4), (5, 6)]))
     assert None == ffirst([])
 
 
@@ -64,6 +64,30 @@ def test_third():
     # when iterable is shorter than Three
     assert 2 == third([1, 2])
 
+
+def test_fourth():
+    assert 4 == fourth([1, 2, 3, 4])
+    assert 4 == fourth((1, 2, 3,4))
+    assert 4 == fourth(set([1, 2, 3,4]))
+    assert (4, "d") == fourth({1: "a", 2: "b", 3: "c", 4:"d"})
+    assert None == fourth([])
+    # check that the last item is returned
+    # when iterable is shorter than Four
+    assert 2 == fourth([1, 2])
+    assert 3 == fourth([1, 2,3])
+
+
+def test_fifth():
+    assert 5 == fifth([1, 2, 3, 4, 5])
+    assert 5 == fifth((1, 2, 3,4,5))
+    assert 5 == fifth(set([1, 2, 3,4,5]))
+    assert (5, "e") == fifth({1: "a", 2: "b", 3: "c", 4:"d", 5:"e"})
+    assert None == fifth([])
+    # check that the last item is returned
+    # when iterable is shorter than Four
+    assert 2 == fifth([1, 2])
+    assert 3 == fifth([1, 2,3])
+    assert 4 == fifth([1, 2,3,4])
 
 def test_butlast():
     assert (1, 2) == butlast([1, 2, 3])
