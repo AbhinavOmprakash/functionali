4c89bc4b37e5fb919a10a8cd3ead2f5d6c446014
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2021-07-15 15:29:35 +0530

    removed dict_functions import

--- functionali/__init__.py
+++ functionali/__init__.py
@@ -1,5 +1,4 @@
-from .dict_functions import *
-from .function_transform import *
+from .higher_order_functions it import *
 from .predicates import *
 from .seq_transform import *
 from .seq_traverse import *
